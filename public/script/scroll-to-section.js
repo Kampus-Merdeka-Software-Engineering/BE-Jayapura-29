575660fd5353edb4c688b1ef2eb8133969e69638
BintangRF <bintangrama777@gmail.com>
2023-10-09 21:00:54 +0700

    hapus folder FE

--- public/script/scroll-to-section.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sectionToScroll = document.getElementById("section-to-scroll");
-  if (sectionToScroll) {
-    sectionToScroll.scrollIntoView({ behavior: "smooth" });
-  }
-});
